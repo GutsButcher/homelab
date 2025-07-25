f71b8bf562d567610ec443a2edb0e5c08ebd3eef
abdulla <abdulla.amash@gmail.com>
2025-07-25 16:38:07 +0300

    remove email from secert resource, add .env.email to vaules

--- apps/pgadmin/prod/pgadmin/create-secret.sh
+++ apps/pgadmin/prod/pgadmin/create-secret.sh
@@ -2,5 +2,4 @@
 # Create the secret with your credentials
 kubectl create secret generic pgadmin-credentials \
   --namespace=pgadmin \
-  --from-literal=email="example@gmail.com" \
   --from-literal=password="SuperSecretPassowrd"
